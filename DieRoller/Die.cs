fa584cfc66d1b1f63a26dd910d4ef8b306ae288a
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-16 08:06:31 -0700

    Add project files.

---
+++ DieRoller/Die.cs
@@ -0,0 +1,31 @@
+﻿namespace DieRoller
+{
+    /// <summary>
+    /// Represents a single six sided die (1 - 6).
+    /// </summary>
+    public class Die
+    {
+        /// <summary>
+        /// The current face up value of the die
+        /// </summary>
+        public byte FaceValue { get; set; }
+
+        /// <summary>
+        /// True if the die is currnetly held
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Rolls the die and sets the <see cref="FaceValue" />
+        /// to the new number, Returns the new face value.
+        /// </summary>
+        /// <return>Returns the new random number</return>
+        public byte Roll()
+        {
+            // Generate a random numbber
+            // set the face value
+            // return random number
+            throw new NotImplementedException();
+        }
+    }
+}
